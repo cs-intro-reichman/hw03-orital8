9dee42982f1bf197eff82a71db3bd509662374e1
omrihruni <154349572+omrihruni@users.noreply.github.com>
2024-01-14 22:39:20 +0200

    Update UniqueChars.java

--- UniqueChars.java
+++ UniqueChars.java
@@ -10,15 +10,16 @@ public class UniqueChars {
      * except that all the duplicate characters are removed,
      * unless they are space characters.
      */
+	// #feedback - please make the code cleaner by removing redundant comments, empty lines and spaces. Also make sure to have proper indentation.
     public static String uniqueChars(String s) {
 //The string input "s"
 
-		String ogS = s ;
+		String ogS = s ; // #feedback - no need to have a new variable here, you can use s directly.
 		String newS = "" ;
 		newS= newS + ogS.charAt(0);
 		int length = ogS.length()-1	;
 		char c;
-//IsEx boolean virable for checking if character exist.
+//IsEx boolean virable for checking if character exist // #feedback - please note the indentation here and the empty lines that should be removed.
 
 		boolean isEx=false ;
 //for loop with the original string length
@@ -29,12 +30,13 @@ public class UniqueChars {
 //for loop with the original string length
 			char q = newS.charAt(0);
 			int newLength = newS.length()-1;
+				// #feedback - there is no need for another loop here. You can just iterate over the string once.
 			for(int j=0; j <= newLength && isEx==false ; j++){		//the smaller
 			q = newS.charAt(j);	
 				if (c == q){
 					isEx = true;
 				}
-				else;
+				else; // #feedback - else without other commands is not needed.
 			}										//the smaller loop
 				
 				if(isEx == false)
